0d08b1c9ccf5d1b8d127c0dbac940e1103415037
justlera <93049864+justlera@users.noreply.github.com>
2022-05-01 21:20:22 +0300

    Update library.cpp

--- library.cpp
+++ library.cpp
@@ -3,78 +3,7 @@
 #include <vector>
 #include <limits>
 #include <list>
-
-using namespace std;
-
-class Book{
-public:
-    virtual bool set_mark(int mark) = 0;
-private:
-    string name;
-    string author;
-    int date;
-};
-
-class PBook : public Book {
-private:
-    int pages;
-    int mark;
-    int state;
-public:
-    // проставляем оценку с требуемыми пределами
-    bool set_mark(int mark) {
-        if (mark < 1 || mark > 10) {
-            return false;
-        }
-        else {
-            this->mark = mark;
-            return true;
-        }
-    }
-
-    PBook(const string& name, const string& author, int date, int pages, int mark, int state)
-        : name(name), author(author), date(date), pages(pages), mark(mark), state(state) {}
-
-};
-
-class Ebook:public Book {
-private:
-    int volume;
-    //std::vector<int> dates;
-    int date;
-    int mark;
-public:
-    // то же, но пределы другие
-    bool set_mark(int mark) {
-        if (mark < 1 || mark > 100) {
-            return false;
-        }
-        else {
-            this->mark = mark;
-            return true;
-        }
-    }
-
-    Ebook(const string& name, const string& author, int volume, int date, int mark)
-            : name(name), author(author), date(date), volume(volume), mark(mark) {}
-};
-
-struct Node{
-    Book *pBook;
-    Node *Next;
-};
-
-class Library{
-    Node *Head, *Tail;                                            //Первый элемент и тот что последний
-    int size;                                                     //Число элементов в списке
-public:
-    Library():Head(NULL),Tail(NULL),size(0){};                       //Инициализация элементов в ноль с помощью конструктора
-    ~Library();                                                       //Прототип деструктор: в деструкторе освободим память
-    void Add(Book *pb);                                              //Прототип функции добавления элементов в списов
-    void Show(int temp);                                         //Прототип функции отображения элементов списка
-    int Count();                                                  //Прототип функции возвращающей число элементов в списке
-    void Del(Book *pb);
-};
+#include "library.h"
 
 Library::~Library()
 {
@@ -119,11 +48,12 @@ void Library::Del(Book *pb)
         while (temp->Next && temp->Next->pBook != pb){
             temp = temp->Next;
         }
-        Node*t2 = temp->Next->Next; // сохраняем указатель Next удаляемого элемента
+        Node* t2 = temp->Next->Next; // сохраняем указатель Next удаляемого элемента
         delete temp->Next->pBook; // удаляем книгу; надо использовать delete
         // вместо free, если память выделялась new
         delete temp->Next; // удаляем хранивший книгу элемент списка
         temp->Next = t2->Next; // восстанавливаем целостность списка
+        size--;
 
     }
 }
